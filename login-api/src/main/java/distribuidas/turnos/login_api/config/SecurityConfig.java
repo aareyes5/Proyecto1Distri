22f22879b11f033a3e68b991c2c0bbb03859008a
aareyes5 <aareyes5@espe.edu.ec>
2025-05-29 14:31:02 -0500

    acceso a forntend

--- login-api/src/main/java/distribuidas/turnos/login_api/config/SecurityConfig.java
+++ login-api/src/main/java/distribuidas/turnos/login_api/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*")); // <-- IMPORTANTE
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
